1554841bd3b6363d090ede2686c10c4f4a75f4ed
HPC <mckena@mail.ru>
2022-02-11 15:13:58 +0300

    l6

--- jobparser/pipelines.py
+++ jobparser/pipelines.py
@@ -12,18 +12,42 @@ from pymongo import MongoClient
 class JobparserPipeline:
     def __init__(self):
         client = MongoClient('localhost', 27017)
-        self.mongobase = client.vacancies0302
+        self.mongobase = client.vacancies0902
 
     def process_item(self, item, spider):
-        # salary = self.process_salary(item.get('salary'))
-        # item['salary_min'], item['salary_max'], item['cur'] = salary
-        # del item['salary']
+        salary = self.process_salary(item.get('salary'), spider)
+        item['salary_min'], item['salary_max'], item['cur'] = salary
+        del item['salary']
         collection = self.mongobase[spider.name]
         collection.insert_one(item)
         return item
 
     @staticmethod
-    def process_salary(dirty_salary):
-        min_salary, max_salary, cur = 0, 0, 'руб'
-        # .....
+    def process_salary(dirty_salary, spider):
+        min_salary, max_salary, cur = None, None, None
+        if spider.name == 'hhru':
+            for index, value in enumerate(dirty_salary):
+                if value.strip() == 'от':
+                    min_salary = int(dirty_salary[index + 1].replace('\xa0', ''))
+                if value.strip() == 'до':
+                    max_salary = int(dirty_salary[index + 1].replace('\xa0', ''))
+            if len(dirty_salary) > 1:
+                cur = dirty_salary[-2]
+        if spider.name == 'superjobru':
+            if 'от' in dirty_salary:
+                dirty_salary.remove('\xa0')
+                min_salary = int(dirty_salary[1].replace('\xa0', '').replace('руб.', ''))
+                cur = 'руб.'
+            elif 'до' in dirty_salary:
+                dirty_salary.remove('\xa0')
+                max_salary = int(dirty_salary[1].replace('\xa0', '').replace('руб.', ''))
+                cur = 'руб.'
+            elif '—' in dirty_salary:
+                for item in dirty_salary:
+                    if item == '\xa0':
+                        dirty_salary.remove('\xa0')
+                min_salary = int(dirty_salary[0].replace('\xa0', ''))
+                max_salary = int(dirty_salary[2].replace('\xa0', ''))
+                cur = dirty_salary[-1]
+
         return min_salary, max_salary, cur
--- jobparser/spiders/superjobru.py
+++ jobparser/spiders/superjobru.py
@@ -1,10 +1,27 @@
 import scrapy
+from scrapy.http import HtmlResponse
+from jobparser.items import JobparserItem
 
 
 class SuperjobruSpider(scrapy.Spider):
     name = 'superjobru'
     allowed_domains = ['superjob.ru']
-    start_urls = ['http://superjob.ru/']
+    start_urls = ['https://russia.superjob.ru/vacancy/search/?keywords=Python']
 
-    def parse(self, response):
-        pass
+    def parse(self, response: HtmlResponse):
+
+        next_page = response.xpath("///a[contains(@class,'dalshe')]/@href").get()
+        if next_page:
+            yield response.follow(next_page, callback=self.parse)
+
+        links = response.xpath("//div[contains(@class,'vacancy-item')]//a[contains(@href, '/vakansii/')]/@href").getall()
+        for link in links:
+            yield response.follow(link, callback=self.vacancy_parse)
+
+    @staticmethod
+    def vacancy_parse(response: HtmlResponse):
+        name = response.xpath("//h1//text()").get()
+        salary = response.xpath("//div[contains(@class, 'vacancy-base-info')]/*/*/*/*/span/span[1]//text()").getall()
+        url = response.url
+        yield JobparserItem(name=name, salary=salary, url=url)
+
